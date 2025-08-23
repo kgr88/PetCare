a259296479d6691343511e96a5fbe6dba189bfee
kgr88 <krystiangruszecki50@gmail.com>
2025-08-23 18:49:33 +0200

    add medication tables

---
+++ PetCare.Server/Models/Medication.cs
@@ -0,0 +1,13 @@
+﻿namespace PetCare.Server.Models;
+
+public class Medication
+{
+    public int Id { get; set; }
+    public int AnimalId { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string? Dosage { get; set; }
+    public string? Instructions { get; set; }
+
+    public MedicationSchedule Schedule { get; set; } = null!;
+
+}
---
+++ PetCare.Server/Models/MedicationLog.cs
@@ -0,0 +1,10 @@
+﻿namespace PetCare.Server.Models;
+
+public class MedicationLog
+{
+    public int Id { get; set; }
+    public int ScheduleId { get; set; }
+    public DateTime TimeTaken { get; set; }
+    public MedicationSchedule MedicationSchedule { get; set; } = null!;
+
+}
