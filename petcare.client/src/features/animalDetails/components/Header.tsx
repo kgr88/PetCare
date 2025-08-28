ba763d9dca734d449dcdcb9607597bdc6f3fdb33
kgr88 <krystiangruszecki50@gmail.com>
2025-08-28 05:36:59 +0200

    fix animal header styling

--- petcare.client/src/features/animalDetails/components/Header.tsx
+++ petcare.client/src/features/animalDetails/components/Header.tsx
@@ -1,25 +1,41 @@
 import type { Animal } from '../../../types';
 import dogSrc from '../../../assets/dog.jpg';
+import { PawPrint, Tag, Calendar, ScanBarcode } from 'lucide-react';
 
 export default function Header({ animal }: { animal?: Animal }) {
   if (!animal) return null;
   return (
-    <div className="flex flex-row md:flex-col items-start">
+    <div className="flex flex-row md:flex-col items-start mb-4">
       <img
         src={dogSrc}
         alt="animal"
-        className="w-40 h-40 rounded-md object-cover shadow-lg"
+        className="w-40 h-40 lg:w-60 lg:h-60 rounded-md object-cover shadow-lg"
       />
-      <div className="mt-4">
+      <div className="ml-2 md:ml-0 md:mt-2">
         <h1 className="text-2xl font-semibold">{animal.name}</h1>
 
-        <div className="mt-2 text-sm text-gray-600 dark:text-gray-300">
-          <div>{animal.species}</div>
-          {animal.breed ? <div>{animal.breed}</div> : null}
-          {animal.dateOfBirth ? <div>Born: {animal.dateOfBirth}</div> : null}
-          {animal.microchipId ? (
-            <div>Microchip: {animal.microchipId}</div>
-          ) : null}
+        <div className="mt-2 text-sm text-gray-600 dark:text-gray-300 flex flex-col gap-2">
+          <div className="flex items-center">
+            <PawPrint className="h-5 mr-1" />
+            <span>Species: {animal.species}</span>
+          </div>
+          {animal.breed && (
+            <div className="flex">
+              <Tag className="h-5 mr-1" />
+              Breed: {animal.breed}
+            </div>
+          )}
+          {animal.dateOfBirth && (
+            <div className="flex">
+              <Calendar className="h-5 mr-1" /> Born: {animal.dateOfBirth}
+            </div>
+          )}
+          {animal.microchipId && (
+            <div className="flex">
+              <ScanBarcode className="h-5 mr-1" /> Microchip:{' '}
+              {animal.microchipId}
+            </div>
+          )}
         </div>
       </div>
     </div>
