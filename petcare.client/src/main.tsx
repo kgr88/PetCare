96e399388750039f124c74716f98dbe27e93fc0e
kgr88 <krystiangruszecki50@gmail.com>
2025-09-04 00:49:24 +0200

    Change page title and remove strictmode

--- petcare.client/src/main.tsx
+++ petcare.client/src/main.tsx
@@ -1,11 +1,6 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './app/App.tsx';
 import '@fontsource/roboto';
 import '@fontsource/roboto/700.css';
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById('root')!).render(<App />);
